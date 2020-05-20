2c6cbaab9ff308ffca7a8c4813cd4ae111022b7e
Jens Henrik Vogelius <jens.vogelius@isobar.com>
2020-05-20 08:33:20 +0200

    Basic beer/batch model and admin config

--- beerlog/log/admin.py
+++ beerlog/log/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Beer, Batch
 
 # Register your models here.
+
+class BatchInline(admin.TabularInline):
+    verbose_name = 'Batch'
+    verbose_name_plural = 'Batches'
+    extra = 0
+    model = Batch
+    fields = ('brew_date', 'og', 'fg')
+    
+    def has_change_permission(self, request, obj=None) -> bool:
+        return False
+
+    def has_delete_permission(self, request, obj=None) -> bool:
+        return False
+
+class LogAdmin(admin.ModelAdmin):
+    inlines= [
+            BatchInline
+            ]
+
+admin.site.register(Beer, LogAdmin)
--- beerlog/log/models.py
+++ beerlog/log/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+
+class Beer(models.Model):
+    name = models.CharField(max_length=200)
+    style = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Batch(models.Model):
+    beer = models.ForeignKey(Beer, on_delete=models.CASCADE)
+    brew_date = models.DateField('date brewed', default=date.today)
+    bottle_date = models.DateField('date bottled', default=date.today)
+    og = models.IntegerField(default=1000)
+    fg = models.IntegerField(default=1000)
+    ibu = models.IntegerField(default=0)
+
+    def calculate_abv(self) -> float:
+        return (self.og - self.fg) * 0.13125
